89e6585a627a92d297401c54175d8b19fee6f157
Maayan Matsliah <maayan.matsliah@gmail.com>
2026-05-08 22:28:14 -0400

    new ingesting of data

--- citation_parser.py
+++ citation_parser.py
@@ -5,14 +5,19 @@ import boto3
 from botocore import UNSIGNED
 from botocore.config import Config
 
-con = duckdb.connect("citations.duckdb")
-con.execute("CREATE TABLE IF NOT EXISTS works (id TEXT, title TEXT, year INT)")
+con = duckdb.connect("data/citations.duckdb")
+con.execute("CREATE TABLE IF NOT EXISTS works (id TEXT, title TEXT, year INT, field TEXT)")
 con.execute("CREATE TABLE IF NOT EXISTS citations (citing_id TEXT, cited_id TEXT)")
 
 s3 = boto3.client("s3", config=Config(signature_version=UNSIGNED))
 
-# grab 20 folders to start
 folders = [
+    "data/works/updated_date=2022-10-20/",
+    "data/works/updated_date=2022-11-02/",
+    "data/works/updated_date=2023-12-11/",
+    "data/works/updated_date=2024-07-19/",
+    "data/works/updated_date=2024-08-27/",
+    "data/works/updated_date=2024-11-08/",
     "data/works/updated_date=2025-07-15/",
     "data/works/updated_date=2025-07-16/",
     "data/works/updated_date=2025-07-22/",
@@ -27,108 +32,6 @@ folders = [
     "data/works/updated_date=2025-07-31/",
     "data/works/updated_date=2025-08-04/",
     "data/works/updated_date=2025-08-18/",
-    "data/works/updated_date=2025-08-27/",
-    "data/works/updated_date=2025-08-28/",
-    "data/works/updated_date=2025-08-29/",
-    "data/works/updated_date=2025-09-03/",
-    "data/works/updated_date=2025-09-08/",
-    "data/works/updated_date=2025-09-09/",
-    "data/works/updated_date=2025-09-23/",
-    "data/works/updated_date=2025-09-24/",
-    "data/works/updated_date=2025-09-25/",
-    "data/works/updated_date=2025-09-26/",
-    "data/works/updated_date=2025-09-27/",
-    "data/works/updated_date=2025-09-28/",
-    "data/works/updated_date=2025-09-29/",
-    "data/works/updated_date=2025-09-30/",
-    "data/works/updated_date=2025-10-01/",
-    "data/works/updated_date=2025-10-02/",
-    "data/works/updated_date=2025-10-03/",
-    "data/works/updated_date=2025-10-04/",
-    "data/works/updated_date=2025-10-05/",
-    "data/works/updated_date=2025-10-06/",
-    "data/works/updated_date=2025-10-07/",
-    "data/works/updated_date=2025-10-08/",
-    "data/works/updated_date=2025-10-09/",
-    "data/works/updated_date=2025-10-10/",
-    "data/works/updated_date=2025-10-11/",
-    "data/works/updated_date=2025-10-12/",
-    "data/works/updated_date=2025-10-13/",
-    "data/works/updated_date=2025-10-14/",
-    "data/works/updated_date=2025-10-15/",
-    "data/works/updated_date=2025-10-16/",
-    "data/works/updated_date=2025-10-17/",
-    "data/works/updated_date=2025-10-18/",
-    "data/works/updated_date=2025-10-19/",
-    "data/works/updated_date=2025-10-20/",
-    "data/works/updated_date=2025-10-21/",
-    "data/works/updated_date=2025-10-22/",
-    "data/works/updated_date=2025-10-23/",
-    "data/works/updated_date=2025-10-24/",
-    "data/works/updated_date=2025-10-25/",
-    "data/works/updated_date=2025-10-26/",
-    "data/works/updated_date=2025-10-27/",
-    "data/works/updated_date=2025-10-28/",
-    "data/works/updated_date=2025-10-29/",
-    "data/works/updated_date=2025-10-30/",
-    "data/works/updated_date=2025-10-31/",
-    "data/works/updated_date=2025-11-01/",
-    "data/works/updated_date=2025-11-02/",
-    "data/works/updated_date=2025-11-03/",
-    "data/works/updated_date=2025-11-04/",
-    "data/works/updated_date=2025-11-05/",
-    "data/works/updated_date=2025-11-06/",
-    "data/works/updated_date=2025-11-07/",
-    "data/works/updated_date=2025-11-08/",
-    "data/works/updated_date=2025-11-09/",
-    "data/works/updated_date=2025-11-10/",
-    "data/works/updated_date=2025-11-11/",
-    "data/works/updated_date=2025-11-12/",
-    "data/works/updated_date=2025-11-13/",
-    "data/works/updated_date=2025-11-14/",
-    "data/works/updated_date=2025-11-15/",
-    "data/works/updated_date=2025-11-16/",
-    "data/works/updated_date=2025-11-17/",
-    "data/works/updated_date=2025-11-18/",
-    "data/works/updated_date=2025-11-19/",
-    "data/works/updated_date=2025-11-20/",
-    "data/works/updated_date=2025-11-23/",
-    "data/works/updated_date=2025-11-25/",
-    "data/works/updated_date=2025-11-27/",
-    "data/works/updated_date=2025-11-28/",
-    "data/works/updated_date=2025-11-29/",
-    "data/works/updated_date=2025-11-30/",
-    "data/works/updated_date=2025-12-01/",
-    "data/works/updated_date=2025-12-02/",
-    "data/works/updated_date=2025-12-03/",
-    "data/works/updated_date=2025-12-04/",
-    "data/works/updated_date=2025-12-05/",
-    "data/works/updated_date=2025-12-06/",
-    "data/works/updated_date=2025-12-07/",
-    "data/works/updated_date=2025-12-08/",
-    "data/works/updated_date=2025-12-09/",
-    "data/works/updated_date=2025-12-10/",
-    "data/works/updated_date=2025-12-11/",
-    "data/works/updated_date=2025-12-12/",
-    "data/works/updated_date=2025-12-13/",
-    "data/works/updated_date=2025-12-14/",
-    "data/works/updated_date=2025-12-15/",
-    "data/works/updated_date=2025-12-16/",
-    "data/works/updated_date=2025-12-17/",
-    "data/works/updated_date=2025-12-18/",
-    "data/works/updated_date=2025-12-19/",
-    "data/works/updated_date=2025-12-20/",
-    "data/works/updated_date=2025-12-21/",
-    "data/works/updated_date=2025-12-22/",
-    "data/works/updated_date=2025-12-23/",
-    "data/works/updated_date=2025-12-24/",
-    "data/works/updated_date=2025-12-25/",
-    "data/works/updated_date=2025-12-26/",
-    "data/works/updated_date=2025-12-27/",
-    "data/works/updated_date=2025-12-28/",
-    "data/works/updated_date=2025-12-29/",
-    "data/works/updated_date=2025-12-30/",
-    "data/works/updated_date=2025-12-31/",
 ]
 
 total_kept = 0
@@ -156,7 +59,10 @@ for folder in folders:
                 title = work.get("title", "")
                 refs = work.get("referenced_works", [])
 
-                con.execute("INSERT INTO works VALUES (?, ?, ?)", [work_id, title, year])
+                topics = work.get("topics", [])
+                field = topics[0]["field"]["display_name"] if topics and "field" in topics[0] else "Unknown"
+
+                con.execute("INSERT INTO works VALUES (?, ?, ?, ?)", [work_id, title, year, field])
                 for ref in refs:
                     con.execute("INSERT INTO citations VALUES (?, ?)", [work_id, ref])
 
